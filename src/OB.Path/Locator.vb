5c5c5bf8b347a8a57017fdb6c98cdbca7739470b
Ryan O'Day <romanguy722@gmail.com>
2015-12-24 22:49:56 -0500

    Add GetAppPath()

--- src/OB.Path/Locator.vb
+++ src/OB.Path/Locator.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Locator
 
+    Public Shared Function GetAppPath() As String
+        Dim i As Integer
+        Dim strAppPath As String
+        strAppPath = System.Reflection.Assembly.GetExecutingAssembly.Location()
+        i = strAppPath.Length - 1
+        Do Until strAppPath.Substring(i, 1) = "\"
+            i = i - 1
+        Loop
+        strAppPath = strAppPath.Substring(0, i)
+        Return strAppPath
+    End Function
+
 End Class
