cf3a3a98262fe63268dc6e14acc45fa0d5a0adaf
Ryan O'Day <romanguy722@gmail.com>
2016-01-05 19:35:26 -0500

    AddOptions()

--- src/Ozone.Browser/Classes/Tabs.vb
+++ src/Ozone.Browser/Classes/Tabs.vb
@@ -3,4 +3,10 @@ Imports WeifenLuo.WinFormsUI.Docking
 
 Public Class Tabs
 
+    Public Shared Sub AddOptions()
+        Dim newtab As New Tab_Options
+        newtab.Show(frmManager.DockPanel1, DockState.Document)
+        frmManager.SetActiveForm(newtab)
+    End Sub
+
 End Class
