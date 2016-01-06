7d720ae3872f5dea88cbc8dd1bcea2a53018efc5
Ryan O'Day <romanguy722@gmail.com>
2016-01-05 19:22:50 -0500

    Switch to Awesomium

--- src/Ozone.Browser/Forms/Tabs/Tab_Browser.Designer.vb
+++ src/Ozone.Browser/Forms/Tabs/Tab_Browser.Designer.vb
@@ -22,17 +22,30 @@ Partial Class Tab_Browser
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.WebControl1 = New Awesomium.Windows.Forms.WebControl(Me.components)
         Me.SuspendLayout()
+        '
+        'WebControl1
+        '
+        Me.WebControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebControl1.Location = New System.Drawing.Point(0, 0)
+        Me.WebControl1.Size = New System.Drawing.Size(284, 261)
+        Me.WebControl1.TabIndex = 0
         '
         'Tab_Browser
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.WebControl1)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Tab_Browser"
-        Me.Text = "Tab_Browser"
+        Me.Text = "New Tab"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Private WithEvents WebControl1 As Awesomium.Windows.Forms.WebControl
 End Class
