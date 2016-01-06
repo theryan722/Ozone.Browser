939931a429c46b5841fc7b93ded337b90fabc974
Ryan O'Day <romanguy722@gmail.com>
2016-01-05 19:36:42 -0500

    AddExtensions()

--- src/Ozone.Browser/Classes/Tabs.vb
+++ src/Ozone.Browser/Classes/Tabs.vb
@@ -3,6 +3,12 @@ Imports WeifenLuo.WinFormsUI.Docking
 
 Public Class Tabs
 
+    Public Shared Sub AddExtensions()
+        Dim newtab As New Tab_Extensions
+        newtab.Show(frmManager.DockPanel1, DockState.Document)
+        frmManager.SetActiveForm(newtab)
+    End Sub
+
     Public Shared Sub AddHelp()
         Dim newtab As New Tab_Help
         newtab.Show(frmManager.DockPanel1, DockState.Document)
