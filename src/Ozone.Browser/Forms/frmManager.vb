cbba540c08cf719d2021236a27cfb3d74bc23d52
Ryan O'Day <romanguy722@gmail.com>
2016-01-06 01:05:01 -0500

    Handle pnl_extendedmenu LostFocus() event

--- src/Ozone.Browser/Forms/frmManager.vb
+++ src/Ozone.Browser/Forms/frmManager.vb
@@ -8,10 +8,22 @@
 
 #End Region
 
+#Region "pnl_extendedmenu"
+
+    Private Sub pnl_menuextended_LostFocus(sender As Object, e As EventArgs) Handles pnl_menuextended.LostFocus
+        pnl_menuextended.Hide()
+    End Sub
+
+#End Region
+
 #Region "frmManager"
 
     Private Sub frmManager_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Manager.InitializeBrowser()
+    End Sub
+
+    Private Sub pnl_menuextended_Paint(sender As Object, e As PaintEventArgs) Handles pnl_menuextended.Paint
+
     End Sub
 
 #End Region
