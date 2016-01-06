89fc82f8e9f6b8bc4276fbb7c84f27ea42820d58
Ryan O'Day <romanguy722@gmail.com>
2016-01-06 00:36:06 -0500

    AreSameImage()

--- src/Ozone.Browser/Forms/frmManager.vb
+++ src/Ozone.Browser/Forms/frmManager.vb
@@ -1,6 +1,20 @@
-﻿Public Class frmManager
+﻿Imports System.Drawing.Imaging
+Imports System.IO
+
+Public Class frmManager
 
 #Region "Methods"
+
+    Public Function AreSameImage(ByVal I1 As Image, ByVal I2 As Image) As Boolean
+        Dim MS1 As New MemoryStream
+        Dim MS2 As New MemoryStream
+        I1.Save(MS1, ImageFormat.Bmp)
+        I2.Save(MS2, ImageFormat.Bmp)
+        For I As Integer = 0 To CInt(MS1.Length) - 1
+            If MS1.ReadByte() <> MS2.ReadByte Then Return False
+        Next
+        Return True
+    End Function
 
     Public Sub SetActiveForm(ByVal doc As XDockContent)
         doc.Activate()
