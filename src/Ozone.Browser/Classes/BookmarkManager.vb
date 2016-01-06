12a4bdb1199dd69261219fe797aa841eb7e32860
Ryan O'Day <romanguy722@gmail.com>
2016-01-06 00:28:46 -0500

    Outline for BookmarkManager

--- src/Ozone.Browser/Classes/BookmarkManager.vb
+++ src/Ozone.Browser/Classes/BookmarkManager.vb
@@ -1,3 +1,15 @@
 ﻿Public Class BookmarkManager
 
+    Public Shared Function IsBookmarked(ByVal url As String) As Boolean
+
+    End Function
+
+    Public Shared Sub AddToBookmarks(ByVal url As String)
+
+    End Sub
+
+    Public Shared Sub RemoveFromBookmarks(ByVal url As String)
+
+    End Sub
+
 End Class
