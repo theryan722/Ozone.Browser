df14b4aaeae7701b5d986c2b5f013c39d099ad05
Ryan O'Day <romanguy722@gmail.com>
2016-01-05 19:35:58 -0500

    AddThemes()

--- src/Ozone.Browser/Classes/Tabs.vb
+++ src/Ozone.Browser/Classes/Tabs.vb
@@ -3,6 +3,12 @@ Imports WeifenLuo.WinFormsUI.Docking
 
 Public Class Tabs
 
+    Public Shared Sub AddThemes()
+        Dim newtab As New Tab_Themes
+        newtab.Show(frmManager.DockPanel1, DockState.Document)
+        frmManager.SetActiveForm(newtab)
+    End Sub
+
     Public Shared Sub AddOptions()
         Dim newtab As New Tab_Options
         newtab.Show(frmManager.DockPanel1, DockState.Document)
