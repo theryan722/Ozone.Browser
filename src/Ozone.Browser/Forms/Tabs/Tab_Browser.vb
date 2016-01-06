770b5e7dbcf1a1a1d98963e35fea5f0b3c927396
Ryan O'Day <romanguy722@gmail.com>
2016-01-05 19:27:10 -0500

    Add regions

--- src/Ozone.Browser/Forms/Tabs/Tab_Browser.vb
+++ src/Ozone.Browser/Forms/Tabs/Tab_Browser.vb
@@ -1,8 +1,24 @@
 ﻿
 Public Class Tab_Browser
 
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "Events"
+
+
+
+#End Region
+
+#Region "Tab_Browser"
+
     Private Sub Tab_Browser_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+#End Region
 
 End Class
